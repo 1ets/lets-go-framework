e3ff11dc97167d5b0677898364ed1d0b5dad1e5f
Dhuta P <dhuta_venturo@loyalto.id>
2022-12-25 19:52:18 +0700

    FEATURE add helpers

---
+++ helpers/json.helper.go
@@ -0,0 +1,12 @@
+package helpers
+
+import (
+	"encoding/json"
+)
+
+// Encode json from object to JSON and beautify the output.
+func ToJson(data interface{}) string {
+	jsonResult, _ := json.MarshalIndent(data, "", " ")
+
+	return string(jsonResult)
+}
