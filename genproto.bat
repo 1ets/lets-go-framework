a3e3376bd7d20c968bd991a70b768c8fde51ed1b
Dhuta Pratama <me@dhutapratama.com>
2022-12-31 10:36:52 +0700

    FIX protobuf structure

--- genproto.bat
+++ genproto.bat
@@ -1,3 +1,3 @@
-protoc --go_out=. --go-grpc_out=. --proto_path=adapters/protobuf/service-account service.account.proto
-protoc --go_out=. --go-grpc_out=. --proto_path=adapters/protobuf/service-transaction service.transaction.proto
-protoc --go_out=. --go-grpc_out=. --proto_path=adapters/protobuf/service-transaction message.transaction.proto
+protoc --go_out=. --go-grpc_out=. --proto_path=adapters/protobuf/service-account api.account.proto
+protoc --go_out=. --go-grpc_out=. --proto_path=adapters/protobuf/service-transaction api.transaction.proto
+protoc --go_out=. --go-grpc_out=. --proto_path=adapters/protobuf/service-transaction format.transaction.proto
