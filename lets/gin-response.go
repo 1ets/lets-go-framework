bbe299954a9cdc85d571092b6b3ed46d06b58863
Dhuta Pratama <me@dhutapratama.com>
2023-01-03 23:51:29 +0700

    FIX gin response structure

--- lets/gin-response.go
+++ lets/gin-response.go
@@ -1,7 +1,11 @@
 package lets
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/status"
 )
 
 type HttpResponse struct {
@@ -14,13 +18,31 @@ func Response(g *gin.Context, i interface{}, err error) {
 	if err != nil {
 		g.JSON(500, HttpResponse{
 			Status:  "error",
-			Message: err.Error(),
+			Message: status.Convert(err).Message(),
 		})
 		return
 	}
 
-	g.JSON(200, HttpResponse{
-		Status: "success",
+	statusCode := http.StatusOK
+	status := "success"
+	if i != nil {
+		var data map[string]interface{}
+		dataJson, _ := json.Marshal(i)
+		json.Unmarshal(dataJson, &data)
+
+		if data["code"] != nil {
+			statusCode = int(data["code"].(float64))
+			i = nil
+		}
+
+		if data["status"] != nil {
+			status = data["status"].(string)
+			i = nil
+		}
+	}
+
+	g.JSON(statusCode, HttpResponse{
+		Status: status,
 		Data:   i,
 	})
 }
