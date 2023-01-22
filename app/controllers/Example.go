dae92311434b5e67b1c9832d6c5073a163029fbb
Dhuta Pratama <dhutapratama@gmail.com>
2023-01-23 00:51:11 +0700

    merge example controllers

--- app/controllers/Example.go
+++ app/controllers/Example.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"lets-go-framework/app/adapters/clients"
 	"lets-go-framework/app/adapters/data"
 	"net/http"
 )
@@ -15,6 +16,34 @@ func Example(request data.RequestExample) (response data.ResponseExample, err er
 
 	response.Code = http.StatusBadRequest
 	response.Status = "format_error"
+
+	return
+}
+
+// gRPC server example controller
+func GrpcServerExample(request data.RequestExample) (response data.ResponseExample, err error) {
+	response.Code = http.StatusCreated
+	response.Status = "success"
+	response.Greeting = fmt.Sprintf("Hello %s! this message was created by grpc server.", request.Name)
+
+	return
+}
+
+// gRPC client example controller
+func GrpcClientExample(request data.RequestExample) (response data.ResponseExample, err error) {
+	response, err = clients.GrpcExample.Greeting(&request)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// RabbitMQ consumer example controller
+func RabbitConsumerExample(request data.RequestExample) (response data.ResponseExample, err error) {
+	response.Code = http.StatusCreated
+	response.Status = "success"
+	response.Greeting = fmt.Sprintf("Hello %s! this message was created by rabbit consumer.", request.Name)
 
 	return
 }
