05cce3a6c4be36e84f9fc164159ca1e1db5225f2
Dhuta Pratama <dhutapratama@gmail.com>
2023-01-23 20:59:53 +0700

    REMOVE unused models

--- app/models/Transaction.table.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Transaction struct {
-	ID        uint    `json:"id" gorm:"column:id;type:autoIncrement;primaryKey"`
-	AccountId uint    `json:"account_id" gorm:"column:account_id"`
-	Flow      string  `json:"flow" gorm:"column:flow"`
-	Amount    float64 `json:"amount" gorm:"column:amount;default:0"`
-}
