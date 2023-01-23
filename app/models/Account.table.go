e823a52538a632acc906918b64a2fb9b81e11b7b
Dhuta Pratama <dhutapratama@gmail.com>
2023-01-23 20:59:53 +0700

    REMOVE unused models

--- app/models/Account.table.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Account struct {
-	ID      uint    `json:"id" gorm:"column:id;type:autoIncrement;primaryKey"`
-	Name    string  `json:"name" gorm:"column:name"`
-	Balance float64 `json:"balance" gorm:"column:balance;default:0"`
-}
